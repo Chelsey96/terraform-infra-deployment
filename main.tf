2d0da51400db6aead53b8245402188ce85ea1750
chelsey96 <chelsea15ndive@gmail.com>
2024-03-20 15:11:57 -0600

    corrected ami

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 
 resource "aws_instance" "jetblue-prod" {
     instance_type = var.prod_instance_type
+    subnet_id = "subnet-09fd4ac937f15cb17"
     ami = var.prod_ami
     tags = {
         name = var.prod_instance_tag
